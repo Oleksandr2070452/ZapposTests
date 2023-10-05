2e7047470163ad7f102b33999d179095fd9e6583
Oleksandr Kraglyk <sanek2070452@gmail.com>
2023-10-05 09:21:01 +0300

    First commit

---
+++ src/test/java/base/BasePage.java
@@ -0,0 +1,47 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class BasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofMillis(15000));
+    }
+
+    public WebElement waitUntilVisibilityOfElement(String locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+    }
+
+    public WebElement waitUntilElementToBeClickable(String locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
+    }
+
+    protected List<WebElement> waitUntilVisibilityOfElementsLocated(String locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+        return driver.findElements(By.xpath(locator));
+    }
+
+    protected List<WebElement> waitUntilElementsToBeClickable(String locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
+        return driver.findElements(By.xpath(locator));
+    }
+
+    public void waitUntilUrlContainsText(String urlPath) {
+        wait.until(ExpectedConditions.urlContains(urlPath));
+    }
+
+    public void waitUntilNumberOfTabToBe(int tabNumber) {
+        wait.until(ExpectedConditions.numberOfWindowsToBe(tabNumber));
+    }
+}
---
+++ src/test/java/tests/TestInit.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestInit {
+
+    WebDriver driver;
+
+    private final String BASE_URL = "https://www.zappos.com/";
+
+    @BeforeMethod
+    public void openDriver() {
+        driver = new ChromeDriver();
+        driver.get(BASE_URL);
+    }
+
+    @AfterMethod
+    public void closeTest() {
+        driver.quit();
+    }
+}
