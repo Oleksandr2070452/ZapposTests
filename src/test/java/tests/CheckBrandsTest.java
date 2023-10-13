44c98097e098aff1396000c285e1727590fe3913
lesiakulyk95 <lesia1995@gmail.com>
2023-10-13 15:32:45 +0300

    Created new CheckBrandsTest QPO4-22

---
+++ src/test/java/tests/CheckBrandsTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import base.TestInit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+import java.util.Locale;
+
+public class CheckBrandsTest extends TestInit {
+
+    @Test
+    public void checkFiltration() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickBrandsButton();
+        String brandLetterToCheck = "d";
+        String brandName = "Diesel";
+
+        Assert.assertTrue(homePage.isBrandsModalWindowDisplayed());
+
+        homePage.clickBrandsByLetter(brandLetterToCheck);
+
+        for (String brand : homePage.getBrandsList()) {
+            Assert.assertTrue(brand.startsWith(brandLetterToCheck.toUpperCase()));
+        }
+        homePage.clickBrandButtonByName(brandName);
+
+        for (String name : homePage.getBrandNamesList(brandName)) {
+            Assert.assertTrue(name.contains(brandName));
+            System.out.println("Product name : " + name + " match with " + brandName);
+        }
+    }
+}
